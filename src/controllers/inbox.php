50a4a5ee38647b7b61b5eb437bba6a3eafeccef3
Visionz73 <crazyvision26@gmail.com>
2025-05-17 23:19:45 +0200

    ff

--- src/controllers/inbox.php
+++ src/controllers/inbox.php
@@ -1,4 +1,9 @@
 <?php
+// Debug-Modus: Alle Fehler anzeigen
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // src/controllers/inbox.php
 
 // 1) DB und Auth laden
