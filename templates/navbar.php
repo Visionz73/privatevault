da7ead37ba58b8487160b9b9600e703bd43af2b2
Visionz73 <crazyvision26@gmail.com>
2025-05-18 00:01:22 +0200

    dd

--- templates/navbar.php
+++ templates/navbar.php
@@ -17,7 +17,7 @@ $user = getUser();
   <div class="flex-1 overflow-y-auto mt-2">
     <!-- Logo -->
     <a href="dashboard.php" class="flex items-center w-full px-4 py-2 mb-6">
-      <img src="/assets/logo.svg" alt="Logo" class="h-8 w-auto" />
+      <img src="/assets/logo.png" alt="Logo" class="h-8 w-auto" />
       <span class="label ml-3 text-xl font-bold text-gray-900">O-Archive</span>
     </a>
     <!-- Navigation Links -->
