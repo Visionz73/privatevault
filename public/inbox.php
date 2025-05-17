d19a6c30a78cfef66c8958f261aa5a76fc9251fe
Visionz73 <crazyvision26@gmail.com>
2025-05-17 23:02:35 +0200

    ff

--- public/inbox.php
+++ public/inbox.php
@@ -2,4 +2,6 @@
 // public/inbox.php
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../src/controllers/inbox.php';
+
+$due_date = $_GET['due_date'] ?? ''; // Default-Wert, falls 'due_date' nicht gesetzt ist
 ?>
