c9292e370cda3d6d5f71ccdef521b5d5f1868c49
Visionz73 <crazyvision26@gmail.com>
2025-05-25 21:26:13 +0200

    dd

--- templates/navbar.php
+++ templates/navbar.php
@@ -424,32 +424,32 @@ function getUserInitials($user) {
     </div>
     <div class="profile-modal-body">
       <nav class="space-y-1">
-        <a href="/privatevault/profile.php" class="modal-menu-item">
+        <a href="/profile.php" class="modal-menu-item">
           <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
           </svg>
           Profil bearbeiten
         </a>
-        <a href="/privatevault/settings.php" class="modal-menu-item">
+        <a href="/settings.php" class="modal-menu-item">
           <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37 2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
           </svg>
           Einstellungen
         </a>
-        <a href="/privatevault/profile.php?tab=notifications" class="modal-menu-item">
+        <a href="/profile.php?tab=notifications" class="modal-menu-item">
           <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-5 5v-5zM9 17H4l5 5v-5zM9 7v10m6-10v10"></path>
           </svg>
           Benachrichtigungen
         </a>
-        <a href="/privatevault/profile.php?tab=security" class="modal-menu-item">
+        <a href="/profile.php?tab=security" class="modal-menu-item">
           <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
           </svg>
           Sicherheit
         </a>
         <hr class="my-3">
-        <a href="/privatevault/logout.php" class="modal-menu-item text-red-600 hover:text-red-700 hover:bg-red-50">
+        <a href="/logout.php" class="modal-menu-item text-red-600 hover:text-red-700 hover:bg-red-50">
           <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
           </svg>
