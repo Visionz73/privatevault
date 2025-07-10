8f1c215e8b7d32e72108617f0699a5838de709c6
Visionz73 <crazyvision26@gmail.com>
2025-07-10 14:21:37 +0200

    d

--- file-explorer.php
+++ file-explorer.php
@@ -160,8 +160,8 @@ function getFileIcon(string $filename): array {
     return $iconMap[$ext] ?? ['icon' => 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z', 'color' => 'text-gray-400'];
 }
 
-// Include the template
-require_once __DIR__ . '/templates/file-explorer.php';
+// Include only the enhanced template
+require_once __DIR__ . '/templates/file-explorer-enhanced.php';
 ?>
 }
 
